9ae9530ca9f320aa3d11ff4632e91bcb744f2c5e
namvfg <duakhongchau1234@gmail.com>
2024-11-11 21:48:21 +0700

    lam lai cho giong thay

--- appdemo/saleapp/app/dao.py
+++ appdemo/saleapp/app/dao.py
@@ -2,62 +2,119 @@ from app.models import Category, Product
 from app import db, app
 
 def load_categories():
-    return Category.query.order_by("id").all()
+    #return Category.query.order_by("id").all()
+    return [{
+        "id": 1,
+        "name": "Mobile"
+    }, {
+        "id": 2,
+        "name": "Tablet"
+    }, {
+        "id": 3,
+        "name": "Laptop"
+    },
+    ]
 
 
 def load_products():
-    query = Product.query
-    return query.all()
-    # products = [{
-    #     "id": 1,
-    #     "name": "iPhone 7 Plus",
-    #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #     "price": 17000000,
-    #     "image":
-    #     "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "id": 2,
-    #     "name": "iPad Pro 2020",
-    #     "description": "Apple, 128GB, RAM: 6GB",
-    #     "price": 37000000,
-    #     "image":
-    #     "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #     "category_id": 2
-    # }, {
-    #     "id": 3,
-    #     "name": "Galaxy Note 10 Plus",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image":
-    #     "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # },
-    #     {
-    #         "id": 4,
-    #         "name": "iPhone 7 Plus",
-    #         "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #         "price": 17000000,
-    #         "image":
-    #             "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #         "category_id": 1
-    #     }, {
-    #         "id": 5,
-    #         "name": "iPad Pro 2020",
-    #         "description": "Apple, 128GB, RAM: 6GB",
-    #         "price": 37000000,
-    #         "image":
-    #             "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #         "category_id": 2
-    #     }, {
-    #         "id": 6,
-    #         "name": "Galaxy Note 10 Plus",
-    #         "description": "Samsung, 64GB, RAML: 6GB",
-    #         "price": 24000000,
-    #         "image":
-    #             "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #         "category_id": 1
-    #     }
-    # ]
+    # query = Product.query
+    # return query.all()
+    return [{
+        "id": 1,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image":
+        "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+        "category_id": 1
+    }, {
+        "id": 2,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image":
+        "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+        "category_id": 2
+    }, {
+        "id": 3,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image":
+        "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+        "category_id": 1
+    }, {
+        "id": 4,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+        "category_id": 1
+    }, {
+        "id": 5,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+        "category_id": 3
+    }, {
+        "id": 6,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+        "category_id": 1
+    }, {
+        "id": 7,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image":
+        "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+        "category_id": 1
+    }, {
+        "id": 8,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image":
+        "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+        "category_id": 2
+    }, {
+        "id": 9,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image":
+        "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+        "category_id": 3
+    }, {
+        "id": 10,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+        "category_id": 3
+    }, {
+        "id": 11,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+        "category_id": 2
+    }, {
+        "id": 12,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+        "category_id": 1
+    }]
 
 
